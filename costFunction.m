6df2bba1559c1eccac5e01489919bfe3d2d863ce
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-07-15 23:18:51 +0200

    costFunction-gradient_step

--- costFunction.m
+++ costFunction.m
@@ -5,9 +5,6 @@ function [J, grad] = costFunction(theta, X, y)
 %   w.r.t. to the parameters.
 % Initialize some useful values
 m = length(y); % number of training examples
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
@@ -17,7 +14,7 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 H = sigmoid(X * theta);
-J = (1/ m) * ((-1 * y' * log(H)) - ((1 - y)' * log(1 - H)));
-gard = 1/ m * (X' * (H - y));
+J = (1/ m) * (-1 * y' * log(H) - (1 - y)' * log(1 - H));
+grad = 1/ m * ((H - y)' * X)';
 % =============================================================
 end
